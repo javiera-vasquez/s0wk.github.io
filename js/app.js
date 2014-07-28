87ca33aed586298aaef8b3149552ba6c7df6994f
Javier Enrique <s0wkete@gmail.com>
2014-07-27 20:03:03 -0400

    Se termina el loop de trabajos con overlay en :hover

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+// Init foudnation library
 $(document).foundation();
+
+// Function of the site
+$( window ).ready(function() {
+    $(".overlay").css("width", $(".fixed-img").width());
+    $(".overlay").css("height", $(".fixed-img").height());
+});
+
+// Resize function for the proyect hover
+$( window ).resize(function() {
+    $(".overlay").css("width", $(".fixed-img").width());
+    $(".overlay").css("height", $(".fixed-img").height());
+});
