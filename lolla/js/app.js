724b3b0c255a649e36faf01f0e77e1ca11eed7d3
Javier Enrique <s0wkete@gmail.com>
2015-01-13 11:24:21 -0300

    lolla test

---
+++ lolla/js/app.js
@@ -0,0 +1,20 @@
+$(document).foundation();
+
+// Know the width of the viewport
+var windowWidth = $(window).width();
+
+window.onload = function() {
+    if(windowWidth >= 650) {
+        $('#program .inline-list .header').css("height", $('#program .inline-list').height());
+    };
+};
+
+$( window ).resize(function() {
+    $('#program .inline-list .header').css("height", $('#program .inline-list li:nth-child(2)').height());
+});
+
+
+
+
+
+
