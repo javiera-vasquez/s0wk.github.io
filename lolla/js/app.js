4f4a07b9db15e2b2dde921fdd0cd0f4e9a7aae16
Javier Enrique <s0wkete@gmail.com>
2015-02-18 17:57:16 -0300

    new version of the site

--- lolla/js/app.js
+++ lolla/js/app.js
@@ -1,17 +1,17 @@
 $(document).foundation();
 
 // Know the width of the viewport
-var windowWidth = $(window).width();
+// var windowWidth = $(window).width();
 
-window.onload = function() {
-    if(windowWidth >= 650) {
-        $('#program .inline-list .header').css("height", $('#program .inline-list').height());
-    };
-};
+// window.onload = function() {
+//     if(windowWidth >= 650) {
+//         $('#program .inline-list .header').css("height", $('#program .inline-list').height());
+//     };
+// };
 
-$( window ).resize(function() {
-    $('#program .inline-list .header').css("height", $('#program .inline-list li:nth-child(2)').height());
-});
+// $( window ).resize(function() {
+//     $('#program .inline-list .header').css("height", $('#program .inline-list li:nth-child(2)').height());
+// });
 
 
 
